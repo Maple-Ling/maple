53bed65610daa1047d6b391ab4ea973e4864c9f4
Maple-Ling <119702711+Maple-Ling@users.noreply.github.com>
2024-12-01 01:03:56 +0800

    更新 vps_tools.sh

--- vps_tools/vps_tools.sh
+++ vps_tools/vps_tools.sh
@@ -4,6 +4,10 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+# 创建快捷命令 m
+echo "alias m='${0}'" >> ~/.bashrc
+source ~/.bashrc
+
 echo -e "${GREEN}Maple工具箱${NC}"
 echo ""
 
@@ -11,7 +15,7 @@ menu() {
     echo "请选择一个分类:"
     echo "(1) 节点搭建"
     echo "(2) WARP工具"
-    echo "(0) 退出"
+    echo "(0) 返回"
     read -p "请输入选项: " choice
     case $choice in
         1)
@@ -20,8 +24,8 @@ menu() {
         2)
             warp_tools
             ;;
-        3)
-            exit 0
+        0)
+            return
             ;;
         *)
             echo "无效的选项，请重新输入！"
@@ -35,15 +39,19 @@ node_setup_tools() {
     echo "节点搭建工具:"
     echo "1) Hysteria2 安装脚本"
     echo "2) Sing-box 安装脚本"
+    echo "(0) 返回"
     read -p "请输入选项: " tool_choice
     case $tool_choice in
         1)
             echo -e "${GREEN}正在下载并运行 Hysteria2 安装脚本...${NC}"
-            wget -N --no-check-certificate https://raw.githubusercontent.com/flame1ce/hysteria2-install/main/hysteria2-install-main/hy2/hysteria.sh && bash hysteria.sh
+            wget -N --no-check-certificate -O hysteria.sh https://raw.githubusercontent.com/flame1ce/hysteria2-install/main/hysteria2-install-main/hy2/hysteria.sh && bash hysteria.sh
             ;;
         2)
             echo -e "${GREEN}正在下载并运行 Sing-box 安装脚本...${NC}"
             bash <(wget -qO- https://raw.githubusercontent.com/fscarmen/sing-box/main/sing-box.sh)
+            ;;
+        0)
+            menu
             ;;
         *)
             echo "无效的选项，请重新输入！"
@@ -51,17 +59,20 @@ node_setup_tools() {
             node_setup_tools
             ;;
     esac
-    menu
 }
 
 warp_tools() {
     echo "WARP工具:"
     echo "1) WARP 安装脚本"
+    echo "(0) 返回"
     read -p "请输入选项: " tool_choice
     case $tool_choice in
         1)
             echo -e "${GREEN}正在下载并运行 WARP 安装脚本...${NC}"
-            wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh && bash menu.sh [option] [license/url/token]
+            wget -N -O menu.sh https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh && bash menu.sh [option] [license/url/token]
+            ;;
+        0)
+            menu
             ;;
         *)
             echo "无效的选项，请重新输入！"
@@ -69,7 +80,6 @@ warp_tools() {
             warp_tools
             ;;
     esac
-    menu
 }
 
 menu
