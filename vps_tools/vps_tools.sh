e477d09cf2825ee0ae2c6e217c388a4f75368ff8
Maple-Ling <119702711+Maple-Ling@users.noreply.github.com>
2024-12-01 02:15:43 +0800

    更新 vps_tools.sh

--- vps_tools/vps_tools.sh
+++ vps_tools/vps_tools.sh
@@ -40,7 +40,8 @@ show_menu() {
     # 输出左侧菜单与右侧菜单对齐
     printf "%-45s %-45s\n" "左侧菜单:" "右侧菜单:"
     printf "%-45s %-45s\n" "$(printf "(%-2s)" 1) 节点搭建" "$(printf "(%-2s)" 2) WARP 工具"
-    printf "%-45s %-45s\n" "$(printf "(%-2s)" 0) 返回上一级" "$(printf "(%-2s)" 99) 退出工具箱"
+    printf "%-45s\n" "$(printf "(%-2s)" 0) 返回上一级"
+    printf "%-45s\n" "$(printf "(%-2s)" 99) 退出工具箱"
 
     echo
 
